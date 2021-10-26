3563a3936a855133a8ac871ceabde6c0aaf25ad3
MattLogan320 <matt.logan320@gmail.com>
2021-10-26 14:25:38 +0100

    added AWS CLI install to file for use with EKS

--- Terraform/EC2/DockerInstall.sh
+++ Terraform/EC2/DockerInstall.sh
@@ -10,8 +10,16 @@ sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-<<<<<<< HEAD
-
-=======
 sudo reboot
->>>>>>> cfcca588e54ff504d01bc8dd9a8e2c7ad3f9a1db
+##For Production Deployment using Kubernetes we need to install the AWS CLI
+#sudo apt install -y unzip
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sudo ./aws/install
+##We then need to install kubectl to buiild our clusters with
+# download
+#curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+# make executable
+#chmod +x kubectl
+# install the binary onto your PATH
+#sudo mv ./kubectl /usr/local/bin/kubectl
